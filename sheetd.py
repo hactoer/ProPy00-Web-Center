ac76871ea610f4610fd5c60407401dda3d6ea974
hactoer <159212182+hactoer@users.noreply.github.com>
2025-02-16 11:23:01 +0800

    Update sheetd.py

--- sheetd.py
+++ sheetd.py
@@ -1,2 +1,13 @@
-import gspread
-from google.oauth2.service_account import Credentials
+# import gspread
+# from google.oauth2.service_account import Credentials
+# import streamlit as st
+# import json
+# class GSH:#GoogleSheetHandler
+#   def __init__(self,sheet):
+#     ServiceAccountInfo=st.secrets['G']
+#     credentials=Credentials.from_service_account_info(ServiceAccountInfo,scope=[st.secrets['scope']['a']]
+#     self.gc=gsread.authorize(credentials)
+#     self.spreadsheet=self.gc.open(sheet)
+#   def RCBN(self,index,colume):#ReadColumnByName
+#     ...
+    
